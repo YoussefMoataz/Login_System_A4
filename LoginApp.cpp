7a322b494ee487a3f53e392f181326a61e934098
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-04-25 17:05:34 +0200

    Added the files.

---
+++ LoginApp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "LoginFunctions.cpp"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    cout << sum(5, 6);
+
+    return 0;
+}
---
+++ LoginFunctions.h
@@ -0,0 +1,6 @@
+#ifndef LOGIN_SYSTEM_A4_LOGINFUNCTIONS_H
+#define LOGIN_SYSTEM_A4_LOGINFUNCTIONS_H
+
+int sum(int x, int y);
+
+#endif
